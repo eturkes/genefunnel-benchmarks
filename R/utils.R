7b2135c3e19c0a980cb8f450eda3e01526c727c3
Emir Turkes <eturkes@bu.edu>
2024-10-01 10:02:18 +0300

    Add R wrapper for GeneFunnel

---
+++ R/utils.R
@@ -0,0 +1,32 @@
+#    This file is part of genefunnel-benchmarks.
+#    Copyright (C) 2024  Emir Turkes, UK DRI at UCL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Emir Turkes can be contacted at emir.turkes@eturkes.com
+
+# This file holds common functions and methods.
+
+genefunnel <- function(mat, gene_sets) {
+
+  if (!inherits(mat, "sparseMatrix")) {
+    mat <- Matrix(mat, sparse = TRUE)
+  }
+
+  score_matrix <- calculateScores(mat, rownames(mat), gene_sets)
+  rownames(score_matrix) <- names(gene_sets)
+  colnames(score_matrix) <- colnames(mat)
+
+  return(score_matrix)
+}
